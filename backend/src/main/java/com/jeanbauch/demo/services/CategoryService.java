2e0a49760c3b537c9753facd531397d3b7b57e74
Jean Bauch <jean.bauch06@gmail.com>
2026-02-05 22:11:26 -0300

    DTO

--- backend/src/main/java/com/jeanbauch/demo/services/CategoryService.java
+++ backend/src/main/java/com/jeanbauch/demo/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.jeanbauch.demo.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jeanbauch.demo.dto.CategoryDTO;
 import com.jeanbauch.demo.entities.Category;
 import com.jeanbauch.demo.repositories.CategoryRepository;
 
@@ -14,7 +16,9 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-    public List<Category> findAll() {
-        return repository.findAll();
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
     }
 }
